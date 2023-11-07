b3eb720bb29b318b257c31bac197556f19e6bfb7
SkepparAbbe <albske12@gmail.com>
2023-11-07 17:00:55 +0100

    Created tables and relations for the database

---
+++ backend/src/main/resources/postgresql/tables.sql
@@ -0,0 +1,46 @@
+-- ENTITIES
+
+CREATE TABLE User(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    phone_number CHAR(10) NOT NULL,
+    user_name TEXT NOT NULL,
+    user_nick TEXT NOT NULL
+);
+
+CREATE TABLE UserGroup (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    group_name TEXT NOT NULL,
+    year CHAR(2),
+    UNIQUE(group_name, year)
+);
+
+CREATE TABLE Product (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    price INT NOT NULL
+);
+
+
+-- RELATIONS
+
+CREATE TABLE Transaction (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user REFERENCES User(id),
+    product REFERENCES Product(id)
+    transaction_time TIME NOT NULL,
+    transaction_date DATE NOT NULL
+);
+
+CREATE TABLE UserInGroup (
+    user REFERENCES User(id),
+    userGroup REFERENCES UserGroup(id),
+    saldo INT NOT NULL,
+    PRIMARY KEY(user, userGroup)
+);
+
+CREATE TABLE ProductInUserGroup (
+    userGroup REFERENCES UserGroup(),
+    product REFERENCES Product(id),
+    PRIMARY KEY(userGroup, product),
+    amount INT
+);
