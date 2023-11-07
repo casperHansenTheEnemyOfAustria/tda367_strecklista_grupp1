5634c552847a58d984d4563ff021fccf5fc6c922
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2023-11-07 12:47:42 +0100

    %3CmxGraphModel%3E%3Croot%3E%3CmxCell%20id%3D%220%22%2F%3E%3CmxCell%20id%3D%221%22%20parent%3D%220%22%2F%3E%3CmxCell%20id%3D%222%22%20value%3D%22%2BgetAvalibleProducts(String%20sID)%3A%20List%26amp%3Blt%3BProducts%26amp%3Bgt%3B%22%20style%3D%22text%3BstrokeColor%3Dnone%3BfillColor%3Dnone%3Balign%3Dleft%3BverticalAlign%3Dtop%3BspacingLeft%3D4%3BspacingRight%3D4%3Boverflow%3Dhidden%3Brotatable%3D0%3Bpoints%3D%5B%5B0%2C0.5%5D%2C%5B1%2C0.5%5D%5D%3BportConstraint%3Deastwest%3BwhiteSpace%3Dwrap%3Bhtml%3D1%3B%22%20vertex%3D%221%22%20parent%3D%221%22%3E%3CmxGeometry%20x%3D%221275%22%20y%3D%22260%22%20width%3D%22350%22%20height%3D%2230%22%20as%3D%22geometry%22%2F%3E%3C%2FmxCell%3E%3C%2Froot%3E%3C%2FmxGraphModel%3E<fix> added maven dependency for enire pring boot web

--- backend/src/main/java/se/cholmers/backend/BackendApplication.java
+++ backend/src/main/java/se/cholmers/backend/BackendApplication.java
@@ -2,7 +2,12 @@ package se.cholmers.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import se.cholmers.backend.Model;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
 public class BackendApplication {
@@ -11,4 +16,21 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	// @Bean
+	// public UserController userController() {
+	// 	return new UserController(programState());
+	// }
+	// @Bean
+	// public ProgramState programState() {
+	// 	return new ProgramState();
+	// }
+}
+
+
+@Configuration
+class AppConfig {
+    @Bean
+    public ProgramState programState() {
+		return new ProgramState();
+	}
 }
