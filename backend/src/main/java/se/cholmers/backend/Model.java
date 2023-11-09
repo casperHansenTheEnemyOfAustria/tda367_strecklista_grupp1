e752f281c63f5bd1f11a2e389071c392f291eb6a
Simon Westlin Green <simon.green@live.se>
2023-11-09 15:41:58 +0100

    feat: added all functions to ProgramState

--- backend/src/main/java/se/cholmers/backend/Model.java
+++ backend/src/main/java/se/cholmers/backend/Model.java
@@ -17,6 +17,10 @@ public class Model {
         Map<UserGroup, Float> saldo = new HashMap<>();
         private Set<UserGroup> groups = new HashSet<>();
 
+        public User(String userID) {
+
+        }
+
         public User(String name, String nick) {
             this.name = name;
             this.nick = nick;
@@ -34,18 +38,44 @@ public class Model {
             }
             return 0f;
         }
+
+        public Set<Product> getAllProducts() {
+            Set<Product> allProducts = new HashSet<>();
+            for (UserGroup userGroup : groups) {
+                allProducts.addAll(userGroup.getProducts());
+            }
+            return allProducts;
+        }
+
+        public Product getProduct(String productID) {
+            for (Product product : getAllProducts()) {
+                if (product.getID() == productID) {
+                    return product;
+                }
+            }
+            return null;
+        }
     }
 
     private class UserGroup {
-        List<Product> products = new ArrayList<>();
+        Set<Product> products = new HashSet<>();
         Year year;
         String name;
         private OrderHistory orderHistory;
+
+        public Set<Product> getProducts() {
+            return products;
+        }
     }
 
     private class Product {
         String name;
+        String productID;
         Float cost;
+
+        public String getID() {
+            return productID;
+        }
     }
 
     private class OrderHistory {
@@ -80,9 +110,10 @@ public class Model {
     }
 
     private class Cart {
-        Map<Product, Integer> itemsInCart;
+        private Map<Product, Integer> itemsInCart;
 
         public Cart() {
+            itemsInCart = new HashMap<>();
         }
 
         public void addToCart(Product product) {
@@ -94,10 +125,31 @@ public class Model {
             }
             itemsInCart.put(product, ++currentInCart);
         }
+
+        public Map<Product, Integer> getCart() {
+            return itemsInCart;
+        }
+
+        public void removeFromCart(Product product) {
+            Integer currentInCart;
+            try {
+                currentInCart = itemsInCart.get(product);
+            } catch (NullPointerException e) {
+                return;
+            }
+            if (currentInCart > 0) {
+                itemsInCart.put(product, --currentInCart);
+            }
+        }
+
+        public void empty() {
+            this.itemsInCart = new HashMap<>();
+        }
     }
 
     private class ProgramState {
         User currentUser;
+        Cart cart;
 
         public ProgramState(User user) {
             this.currentUser = user;
@@ -107,5 +159,23 @@ public class Model {
             return currentUser.getSaldo(groupID).toString();
         }
 
+        public void addToCart(String productID) {
+            Product product = currentUser.getProduct(productID);
+            cart.addToCart(product);
+        }
+
+        public void removeFromCart(String productID) {
+            Product product = currentUser.getProduct(productID);
+            cart.removeFromCart(product);
+        }
+
+        public Cart getCart() {
+            return cart;
+        }
+
+        public void completePurchase() {
+            cart.empty();
+            return;
+        }
     }
 }
