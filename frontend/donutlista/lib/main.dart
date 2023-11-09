ec67ae25d72a997276647af621d2202e015a10f4
Huml4n <h.forsling@gmail.com>
2023-11-09 18:59:13 +0100

    <feat> Starting to build a dropdown menu

--- frontend/donutlista/lib/main.dart
+++ frontend/donutlista/lib/main.dart
@@ -37,20 +37,45 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout demo',
+      title: 'Menubar',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: const Center(
-          child: MainItemGrid(),
-        )
+        appBar: AppBar(title: const Text('Flutter layout demo')),
+        body: const DropdownMenuMain()
       ),
     );
   }
-  
 }
 
+// Menu bar setup 
+
+const List<String> list = <String>['Strecklista','Inventarier','Transaktioner', 'Användare', 'Inställningar'];
+
+class DropdownMenuMain extends StatefulWidget {
+  const DropdownMenuMain({super.key});
+
+  @override
+  State<DropdownMenuMain> createState() => _DropdownMenuMainState();
+}
+
+class _DropdownMenuMainState extends State<DropdownMenuMain> {
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownMenu<String>(
+      initialSelection: list.first,
+      onSelected: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      dropdownMenuEntries: list.map<DropdownMenuEntry<String>>((String value) {
+        return DropdownMenuEntry<String>(value: value, label: value);
+      }).toList(),
+    );
+  }
+}
 
 
 
