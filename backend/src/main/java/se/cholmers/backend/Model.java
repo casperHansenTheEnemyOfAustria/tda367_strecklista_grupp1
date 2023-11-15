a8830e0e21c94e7c145a29f2a4163554f4c57016
Simon Westlin Green <simon.green@live.se>
2023-11-15 14:54:37 +0100

    fix: added productID to every new product

--- backend/src/main/java/se/cholmers/backend/Model.java
+++ backend/src/main/java/se/cholmers/backend/Model.java
@@ -7,12 +7,14 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
 
 public class Model {
     /**
-     * This class represents a user in the system. It contains information about the user such as their name, nickname and phone number
+     * This class represents a user in the system. It contains information about the
+     * user such as their name, nickname and phone number
      */
     private class User {
         private String name;
@@ -29,14 +31,15 @@ public class Model {
          * 
          * @param name
          * @param nick
-         * this is the constructor
-         * it should be called when a new user is created and also add the user's group to it read from the database
+         *             this is the constructor
+         *             it should be called when a new user is created and also add the
+         *             user's group to it read from the database
          */
         public User(String name, String nick) {
             this.name = name;
             this.nick = nick;
 
-            //logic to add the user to the group
+            // logic to add the user to the group
         }
 
         public void addUserToGroup(UserGroup group) {
@@ -62,7 +65,8 @@ public class Model {
 
         /**
          * 
-         * @return returns a set of all products the user has access to in all its usergorups
+         * @return returns a set of all products the user has access to in all its
+         *         usergorups
          * @throws NullPointerException if the user is not a member of any group
          */
         public Set<Product> getAllProducts() {
@@ -76,7 +80,8 @@ public class Model {
         /**
          * 
          * @param productID
-         * @return the product with the given productID logged in its usergroup's database
+         * @return the product with the given productID logged in its usergroup's
+         *         database
          * @throws NullPointerException if the user is not a member of any group
          */
         public Product getProduct(String productID) {
@@ -98,17 +103,33 @@ public class Model {
         public UserGroup(String name, Year year) {
             this.name = name;
             this.year = year;
-            //code that initializes the object fdrom the database
+            this.orderHistory = new OrderHistory();
+            // code that initializes the object fdrom the database
         }
 
+        public UserGroup(String name, Year year, OrderHistory orderHistory) {
+            this.name = name;
+            this.year = year;
+            this.orderHistory = orderHistory;
+            // code that initializes the object fdrom the database
+        }
 
         /**
+         * First updates the products from the database.
          * 
          * @return the set of products in the usergroup
          */
-
         public Set<Product> getProducts() {
             return products;
+        }
+
+        /**
+         * First updates the history from the database.
+         * 
+         * @return the order history for a group
+         */
+        public OrderHistory getOrderHistory() {
+            return orderHistory;
         }
     }
 
@@ -116,6 +137,18 @@ public class Model {
         private String name;
         private String productID;
         private Float cost;
+
+        /**
+         * Creates a new product with a random ID given the parameters.
+         * 
+         * @param name (the name of the product.)
+         * @param cost (the price of the product in SEK.)
+         */
+        public Product(String name, Float cost) {
+            this.name = name;
+            this.cost = cost;
+            productID = UUID.randomUUID().toString();
+        }
 
         public String getID() {
             return productID;
@@ -223,49 +256,50 @@ public class Model {
             return;
         }
     }
+
     public class StateManager {
-        private Map<String, ProgramState> states = new HashMap<>(); //stateID --> state
+        private Map<String, ProgramState> states = new HashMap<>(); // stateID --> state
 
         public StateManager() {
         }
 
-        public String[] login(String userName, String password){
-            User user =new User(userName, password);
+        public String[] login(String userName, String password) {
+            User user = new User(userName, password);
             ProgramState state = new ProgramState(user);
-    
+
             String stateID = UUID.randomUUID().toString();
             states.put(stateID, state);
-            String[] output = {stateID, getAuth(stateID)};
+            String[] output = { stateID, getAuth(stateID) };
             return output;
         }
-        
-        public String logout(String stateID){
+
+        public String logout(String stateID) {
             states.remove(stateID);
             return "log out successful";
 
         }
 
-        public String getSaldo(String stateID, String groupID){
+        public String getSaldo(String stateID, String groupID) {
             return states.get(stateID).getSaldo(groupID);
         }
 
-        public void addToCart(String stateID, String productID){
+        public void addToCart(String stateID, String productID) {
             states.get(stateID).addToCart(productID);
         }
 
-        public void removeFromCart(String stateID, String productID){
+        public void removeFromCart(String stateID, String productID) {
             states.get(stateID).removeFromCart(productID);
         }
 
-        public Cart getCart(String stateID){
+        public Cart getCart(String stateID) {
             return states.get(stateID).getCart();
         }
 
-        public void completePurchase(String stateID){
+        public void completePurchase(String stateID) {
             states.get(stateID).completePurchase();
         }
 
-        private String getAuth(String stateID){
+        private String getAuth(String stateID) {
             return "test";
         }
     }
