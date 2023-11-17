d2bb3a687fd9b0cb9f1c38db86337e1c3f36dde4
SkepparAbbe <albske12@gmail.com>
2023-11-17 10:10:23 +0100

    :update, worked on db, integrating model

--- backend/src/main/java/se/cholmers/backend/DatabaseInterface.java
+++ backend/src/main/java/se/cholmers/backend/DatabaseInterface.java
@@ -1,42 +1,215 @@
 package se.cholmers.backend;
-import java.sql.*;
-import java.util.Properties;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseInterface {
 
-    // Database connection
-    private Connection conn;
+    private static DatabaseInterface instance;
 
-    //Name the database, can be left blank to use default
-    static final String DBNAME = "";
+    // Replace these with your actual database information
+    private static final String DB_URL = "jdbc:postgresql://your_database_url";
+    private static final String DB_USER = "your_database_user";
+    private static final String DB_PASSWORD = "your_database_password";
 
-    //TODO Information for connecting to the database
+    private Connection connection;
 
-    //URL
-    private static final String DATABASE = "";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "";
-    
+    // Private constructor to enforce singleton pattern
+    private DatabaseInterface() {
+        try {
+            // Load the JDBC driver
+            Class.forName("org.postgresql.Driver");
 
-    public DatabaseInterface() throws SQLException, ClassNotFoundException {
-        this(DATABASE, USERNAME, PASSWORD);
+            // Establish the database connection
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public DatabaseInterface(String db, String user, String password) throws SQLException, ClassNotFoundException {
-        Class.forName("org.postgresql.Driver");
-        Properties props = new Properties();
-        props.setProperty("user", user);
-        props.setProperty("password", password);
-        conn = DriverManager.getConnection(db, props);
+    // Get the singleton instance
+    public static synchronized DatabaseInterface getInstance() {
+        if (instance == null) {
+            instance = new DatabaseInterface();
+        }
+        return instance;
     }
 
-    // Method for sending a query to the database 
+    // ... (previous methods)
 
-    public String request(String request) {
-        try {}
+    // LIST operation
+    public List<Person> getAllPersons() {
+        String sql = "SELECT * FROM Person";
+        List<Person> persons = new ArrayList<>();
+
+        try (PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                Person person = new Person(
+                        resultSet.getInt("id"),
+                        resultSet.getString("phone_number"),
+                        resultSet.getString("person_name"),
+                        resultSet.getString("person_nick")
+                );
+                persons.add(person);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return persons;
     }
-    
 
+    // CRUD operations for Product
 
+    // CREATE operation for Product
+    public void addProduct(Product product) {
+        String sql = "INSERT INTO Product (id, name, price) VALUES (?, ?, ?)";
 
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, product.getId());
+            statement.setString(2, product.getName());
+            statement.setFloat(3, product.getPrice());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // READ operation for Product
+    public Product getProductById(int id) {
+        String sql = "SELECT * FROM Product WHERE id = ?";
+        Product product = null;
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    product = new Product(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getFloat("price")
+                    );
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return product;
+    }
+
+    // UPDATE operation for Product
+    public void updateProduct(Product product) {
+        String sql = "UPDATE Product SET name = ?, price = ? WHERE id = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, product.getName());
+            statement.setFloat(2, product.getPrice());
+            statement.setInt(3, product.getId());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // DELETE operation for Product
+    public void deleteProduct(int id) {
+        String sql = "DELETE FROM Product WHERE id = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // CRUD operations for Committee
+
+    // CREATE operation for Committee
+    public void addCommittee(Committee committee) {
+        String sql = "INSERT INTO Committee (id, group_name, year) VALUES (?, ?, ?)";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, committee.getId());
+            statement.setString(2, committee.getGroupName());
+            statement.setString(3, committee.getYear());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // READ operation for Committee
+    public Committee getCommitteeById(int id) {
+        String sql = "SELECT * FROM Committee WHERE id = ?";
+        Committee committee = null;
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    committee = new Committee(
+                            resultSet.getInt("id"),
+                            resultSet.getString("group_name"),
+                            resultSet.getString("year")
+                    );
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return committee;
+    }
+
+    // UPDATE operation for Committee
+    public void updateCommittee(Committee committee) {
+        String sql = "UPDATE Committee SET group_name = ?, year = ? WHERE id = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, committee.getGroupName());
+            statement.setString(2, committee.getYear());
+            statement.setInt(3, committee.getId());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // DELETE operation for Committee
+    public void deleteCommittee(int id) {
+        String sql = "DELETE FROM Committee WHERE id = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- backend/src/main/resources/postgresql/tables.sql
+++ backend/src/main/resources/postgresql/tables.sql
@@ -1,10 +1,10 @@
 -- ENTITIES
 
-CREATE TABLE User(
+CREATE TABLE Person (
     id INT PRIMARY KEY,
     phone_number CHAR(10) NOT NULL,
-    user_name TEXT NOT NULL,
-    user_nick TEXT NOT NULL
+    person_name TEXT NOT NULL,
+    person_nick TEXT NOT NULL
 );
 
 CREATE TABLE Committee (
@@ -15,7 +15,7 @@ CREATE TABLE Committee (
 );
 
 CREATE TABLE Product (
-    id INT KEY,
+    id INT PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
     price FLOAT NOT NULL
 );
@@ -25,22 +25,22 @@ CREATE TABLE Product (
 
 CREATE TABLE Transaction (
     id INT PRIMARY KEY,
-    user REFERENCES User(id),
-    product REFERENCES Product(id)
+    person_id INT REFERENCES Person(id) NOT NULL,
+    product_id INT REFERENCES Product(id) NOT NULL,
     transaction_time TIME NOT NULL,
     transaction_date DATE NOT NULL
 );
 
-CREATE TABLE UserInCommittee (
-    user REFERENCES User(id),
-    userGroup REFERENCES UserGroup(id),
+CREATE TABLE PersonInCommittee (
+    person_id INT REFERENCES Person(id) NOT NULL,
+    committee_id INT REFERENCES Committee(id) NOT NULL,
     saldo INT NOT NULL,
-    PRIMARY KEY(user, userGroup)
+    PRIMARY KEY(person_id, committee_id)
 );
 
-CREATE TABLE ProductInCommmittee (
-    userGroup REFERENCES UserGroup(),
-    product REFERENCES Product(id),
-    PRIMARY KEY(userGroup, product),
+CREATE TABLE ProductInCommittee (
+    committee_id INT REFERENCES Committee(id) NOT NULL,
+    product_id INT REFERENCES Product(id) NOT NULL,
+    PRIMARY KEY(committee_id, product_id),
     amount INT
 );
