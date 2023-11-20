202fe8008278b9ea65da3b1a27a949741ce7a328
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2023-11-20 15:47:55 +0100

    feat: added db integration to user

--- backend/src/main/java/se/cholmers/backend/Model/User.java
+++ backend/src/main/java/se/cholmers/backend/Model/User.java
@@ -2,19 +2,24 @@ package se.cholmers.backend.Model;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import se.cholmers.backend.DatabaseInterface;
 
 /**
  * This class represents a user in the system. It contains information about the
  * user such as their name, nickname and phone number
  */
 class User {
+    private String id;
     private String name;
     private String nick;
     private String phoneNumber;
     private Map<String, Float> saldo = new HashMap<>();
     private Set<UserGroup> groups = new HashSet<>();
+    private DatabaseInterface dbi = DatabaseInterface.getInstance();
 
     /**
      * This is the constructor for when recreating users from the database
@@ -22,7 +27,24 @@ class User {
      * @param userID
      */
     public User(String userID) {
+        this.id = userID;
+        this.name = dbi.getUserName(userID);
+        this.nick = dbi.getUserNick(userID);
+        this.phoneNumber = dbi.getUserPhoneNumber(userID);
+        addGroupsFromDatabase();
+    }
 
+    /**
+     * This method adds the user's groups to the user from the database
+     * @param userID
+     * 
+     */
+    private void addGroupsFromDatabase() {
+        //TODO add a catch for if there are no groups
+        List<String> comitteeIds = dbi.getCommitteesOfUser(id);
+        for (String param : comitteeIds) {
+            groups.add(new UserGroup(param));
+        }
     }
 
     /**
@@ -44,6 +66,7 @@ class User {
     }
 
     public void addUserToGroup(UserGroup group) {
+        dbi.putUserInCommittee(id, group.getID().toString(), "0");
         groups.add(group);
     }
 
@@ -56,12 +79,9 @@ class User {
      * @throws NullPointerException     if the user is not a member of any group
      */
     public Float getSaldo(String groupID) {
-        for (UserGroup userGroup : groups) {
-            if (userGroup.getID() == groupID) {
-                return saldo.get(groupID);
-            }
-        }
-        return 0f;
+        Float saldoFromDB = dbi.getSaldoFromUserInCommittee(id, groupID);
+        saldo.put(groupID, saldoFromDB);
+        return saldo.get(groupID);
     }
 
     /**
@@ -86,6 +106,7 @@ class User {
      */
     public Set<Product> getAllProducts() {
         Set<Product> allProducts = new HashSet<>();
+        addGroupsFromDatabase();
         for (UserGroup userGroup : groups) {
             allProducts.addAll(userGroup.getProducts());
         }
