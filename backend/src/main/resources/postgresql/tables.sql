ce71df026932695e7d2d5e656d5a7beca99a5aa1
Simon Westlin Green <simon.green@live.se>
2023-11-27 15:14:39 +0100

    Changed some things about database names

--- backend/src/main/resources/postgresql/tables.sql
+++ backend/src/main/resources/postgresql/tables.sql
@@ -29,21 +29,21 @@ CREATE TABLE Products (
 CREATE TABLE Transaction (
     id TEXT PRIMARY KEY,
     user_id TEXT REFERENCES Users(id) NOT NULL,
-    product_id TEXT REFERENCES Product(id) NOT NULL,
+    product_id TEXT REFERENCES Products(id) NOT NULL,
     transaction_time TIME NOT NULL,
     transaction_date DATE NOT NULL
 );
 
 CREATE TABLE UserInCommittee (
-    user_id TEXT REFERENCES User(id) NOT NULL,
-    committee_id TEXT REFERENCES Committee(id) NOT NULL,
+    user_id TEXT REFERENCES Users(id) NOT NULL,
+    committee_id TEXT REFERENCES Committees(id) NOT NULL,
     saldo INT NOT NULL,
     PRIMARY KEY(user_id, committee_id)
 );
 
 CREATE TABLE ProductInCommittee (
-    committee_id TEXT REFERENCES Committee(id) NOT NULL,
-    product_id TEXT REFERENCES Product(id) NOT NULL,
+    committee_id TEXT REFERENCES Committees(id) NOT NULL,
+    product_id TEXT REFERENCES Products(id) NOT NULL,
     PRIMARY KEY(committee_id, product_id)
     
 );
