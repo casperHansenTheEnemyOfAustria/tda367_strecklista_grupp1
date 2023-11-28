c914471735aba06449ae38487a9a3ea04bccb354
Simon Westlin Green <simon.green@live.se>
2023-11-28 14:16:37 +0100

    formatting lol

--- backend/src/main/java/se/cholmers/backend/Interface/IDatabaseInterface.java
+++ backend/src/main/java/se/cholmers/backend/Interface/IDatabaseInterface.java
@@ -1,17 +1,15 @@
 package se.cholmers.backend.Interface;
 
 import java.util.List;
-import java.util.Map;
 
 import se.cholmers.backend.RequestException;
 
 public interface IDatabaseInterface {
 
-
-
     /**
      * Creates a committee in the database
      * precondition: There has to be a database and the committee has to not exist
+     * 
      * @param group_name
      * @param year
      * @return committeeID
@@ -47,7 +45,7 @@ public interface IDatabaseInterface {
      * @return
      * @throws RequestException if the user already exists
      * 
-     * postcondition: The user is created in the database
+     *                          postcondition: The user is created in the database
      */
     public String createUser(String userName, String userNick, String phoneNumber, String committeeID, String saldo,
             String password)
@@ -56,6 +54,7 @@ public interface IDatabaseInterface {
     /**
      * Returns the price of a product
      * precondition: The product has to exist
+     * 
      * @param id
      * @return price
      * @throws NullPointerException if the product does not exist
@@ -65,6 +64,7 @@ public interface IDatabaseInterface {
     /**
      * Returns the amount of a product
      * precondition: The product has to exist
+     * 
      * @param id
      * @return amount
      * @throws NullPointerException if the product does not exist
@@ -74,6 +74,7 @@ public interface IDatabaseInterface {
     /**
      * Returns name of a product
      * precondition: The product has to exists
+     * 
      * @param id
      * @return name
      * @throws NullPointerException if the product does not exist
@@ -83,16 +84,19 @@ public interface IDatabaseInterface {
     /**
      * Returns the user id of a user with a name and a password from the database
      * precondition: The user has to exist
+     * 
      * @param nick
      * @param password
      * @return userID
-     * @throws NullPointerException if a user with the given name and password does not exist
+     * @throws NullPointerException if a user with the given name and password does
+     *                              not exist
      */
     public String getUseridFromName(String nick, String password) throws RequestException;
 
     /**
      * Returns the username of a user
      * precondition: The user has to exist
+     * 
      * @param nick
      * @return username
      * @throws NullPointerException if a user with the given name does not exist
@@ -102,6 +106,7 @@ public interface IDatabaseInterface {
     /**
      * Returns the user nick of a user
      * precondition: The user has to exist
+     * 
      * @param id
      * @return userNick
      * @throws NullPointerException if a user with the given id does not exist
@@ -111,6 +116,7 @@ public interface IDatabaseInterface {
     /**
      * Returns the user phone number of a user
      * precondition: The user has to exist
+     * 
      * @param id
      * @return userPhoneNumber
      * @throws NullPointerException if a user with the given id does not exist
@@ -118,8 +124,9 @@ public interface IDatabaseInterface {
     public String getUserPhoneNumber(String id);
 
     /**
-     * Returns  a list of the committees a user is in
+     * Returns a list of the committees a user is in
      * precondition: The user has to exist
+     * 
      * @param id
      * @return committees
      * @throws NullPointerException if a user with the given id does not exist
@@ -128,11 +135,13 @@ public interface IDatabaseInterface {
 
     /**
      * Returns the particular saldo of a user in that committee
-     * precondition: The user has to exist and be in the committee and the committee has to exist
+     * precondition: The user has to exist and be in the committee and the committee
+     * has to exist
+     * 
      * @param userID
      * @param committeeID
      * @return saldo
-     * @throws NullPointerException if a user with the given id does not exist
+     * @throws NullPointerException     if a user with the given id does not exist
      * @throws IllegalArgumentException if the user is not in the committee
      */
     public Float getSaldoFromUserInCommittee(String userID, String committeeID);
@@ -140,6 +149,7 @@ public interface IDatabaseInterface {
     /**
      * Returns the name of a committee
      * precondition: The committee has to exist
+     * 
      * @param id
      * @return committeeName
      * @throws NullPointerException if a committee with the given id does not exist
@@ -149,6 +159,7 @@ public interface IDatabaseInterface {
     /**
      * Returns the year of a committee
      * precondition: The committee has to exist
+     * 
      * @param id
      * @return committeeYear
      * @throws NullPointerException if a committee with the given id does not exist
@@ -158,6 +169,7 @@ public interface IDatabaseInterface {
     /**
      * Returns the products owned by a committee
      * precondition: The committee has to exist
+     * 
      * @param committeeID
      * @return products
      * @throws NullPointerException if a committee with the given id does not exist
@@ -167,20 +179,25 @@ public interface IDatabaseInterface {
     /**
      * Adds a user committee relationship to the database
      * precondition: The user and the committee has to exist
+     * 
      * @param id
      * @param committeeID
      * @param saldo
-     * @throws NullPointerException if a user with the given id does not exist or the committee does not exist
+     * @throws NullPointerException if a user with the given id does not exist or
+     *                              the committee does not exist
      */
     public void putUserInCommittee(String id, String committeeID, String saldo);
 
     /**
      * Updates the saldo of a user in a committee
-     * precondition: The user and the committee has to exist and the user has to be in the committee
+     * precondition: The user and the committee has to exist and the user has to be
+     * in the committee
+     * 
      * @param id
      * @param committeeId
      * @param saldo
-     * @throws NullPointerException if a user with the given id does not exist or the committee does not exist
+     * @throws NullPointerException     if a user with the given id does not exist
+     *                                  or the committee does not exist
      * @throws IllegalArgumentException if the user is not in the committee
      */
     public void updateUserSaldo(String id, String committeeId, String saldo);
@@ -188,6 +205,7 @@ public interface IDatabaseInterface {
     /**
      * Updates the amount of a product
      * precondition: The product has to exist
+     * 
      * @param productID
      * @param amount
      * @throws NullPointerException if a product with the given id does not exist
