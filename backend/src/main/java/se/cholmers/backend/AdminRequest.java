2b82d69ace953380f97281e4288c8ac987efec2a
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2023-12-04 14:11:36 +0100

    feat: added authentication flag to admin requests

--- backend/src/main/java/se/cholmers/backend/AdminRequest.java
+++ backend/src/main/java/se/cholmers/backend/AdminRequest.java
@@ -1,19 +1,22 @@
 package se.cholmers.backend;
 
+import java.util.Map;
+
 import se.cholmers.backend.Interface.iRequest;
 
 public class AdminRequest implements iRequest {
-    private String data;
+    public String auth;
+    private Map<String, String> data;
 
-    public AdminRequest(String data) {
-
+    public AdminRequest(String auth, Map<String, String> data) {
+        this.auth = auth;
         this.data = data;
     }
 
     @Override
     public String getData(String key) throws RequestException {
 
-        String out = data;
+        String out = data.get(key);
         if (out == null) {
             throw new RequestException("No such key");
         }
