7ce671860722ac014a6f01957a558045beb3bfd7
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2023-12-08 11:29:24 +0100

    Revert "Admin and user access to product amounts"

--- backend/src/main/java/se/cholmers/backend/RequestException.java
+++
@@ -1,7 +0,0 @@
-package se.cholmers.backend;
-
-public class RequestException extends Exception{
-    public RequestException(String message){
-        super(message);
-    }
-}
--- backend/src/main/resources/postgresql/views.sql
+++
@@ -1,6 +0,0 @@
--- Views for easy access of tables
-
-CREATE VIEW TransactionsDescendingDate AS
-SELECT id, user_id, product_id, transaction_time, transaction_date
-FROM Transaction
-ORDER BY transaction_date DESC;
